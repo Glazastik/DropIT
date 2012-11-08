b5cce1463a5b909737179c9cc51dadb8efffe2a1
Anton Myrholm <anton.myrholm@gmail.com>
2012-11-08 22:03:44 +0100

    Merge branch 'master' of https://github.com/Glazastik/DropIT.git

---
+++ Spel1/src/se/chalmers/dropit/spel1/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package se.chalmers.dropit.spel1;
+
+/**
+ * @author Anton Myrholm
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
