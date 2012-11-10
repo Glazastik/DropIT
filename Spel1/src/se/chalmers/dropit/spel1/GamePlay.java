34ec72f0b773da9061d04a3c03f36ff6194cd183
Anton Myrholm <anton.myrholm@gmail.com>
2012-11-10 12:31:08 +0100

    En alpha på ett kasst spel.
    Signed-off-by: Anton Myrholm <anton.myrholm@gmail.com>

---
+++ Spel1/src/se/chalmers/dropit/spel1/GamePlay.java
@@ -0,0 +1,70 @@
+package se.chalmers.dropit.spel1;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class GamePlay extends BasicGameState {
+	private final int ID = 1;
+	private Image world;
+	private Image player;
+	private SpriteSheet sheet;
+	
+	private float x = 400 , y = 300;
+	
+	public GamePlay(int state) {
+		
+	}
+
+	@Override
+	public void init(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		world = new Image("se/chalmers/dropit/spel1/res/land.jpg");
+		player = new Image("se/chalmers/dropit/spel1/res/plane.png");
+		sheet = new SpriteSheet("se/chalmers/dropit/spel1/res/plane.png", );
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		world.draw();
+		player.draw(x,y);
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		Input input = gc.getInput();
+		if (input.isKeyDown(Input.KEY_A)) {
+			player.rotate(-0.2f * delta);
+			
+		}
+		
+		if (input.isKeyDown(Input.KEY_D)) {
+			player.rotate(+0.2f * delta);
+			
+		}
+		
+		if (input.isKeyDown(Input.KEY_W)) {
+			float hip = 0.4f * delta;
+			 
+            float rotation = player.getRotation();
+ 
+            x+= hip * Math.sin(Math.toRadians(rotation));
+            y-= hip * Math.cos(Math.toRadians(rotation));
+			
+		}
+		
+		if (input.isKeyDown(Input.KEY_S)) {
+//			y = (float) (y + delta * .1f);
+		}
+
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return ID;
+	}
+
+}
--- Spel1/src/se/chalmers/dropit/spel1/Main.java
+++ Spel1/src/se/chalmers/dropit/spel1/Main.java
@@ -1,21 +1,55 @@
 /**
- * 
+ * @author Anton Myrholm
+ * @author Pontus Mlam
  */
 package se.chalmers.dropit.spel1;
 
-/**
- * @author Anton Myrholm
- * @author Pontus Mlam
- *
- */
-public class Main {
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class Main extends StateBasedGame {
+
+	private static final String GAMENAME = "Spel 1 - test";
+	private static final int GAMEWINDOW_X = 800;
+	private static final int GAMEWINDOW_Y = 600;
+	private static final int MENU = 0;
+	private static final int GAMEPLAY = 1;
+	private static final boolean FULLSCREENMODE = true;
+
+	public Main(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+//		this.addState(new Menu(MENU));
+		this.addState(new GamePlay(GAMEPLAY));
+	}
 
 	/**
+	 * The main method, it generates the game container window and starts the game in in.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		AppGameContainer appGameContainer;
 
+		try {
+			appGameContainer = new AppGameContainer(new Main(GAMENAME));
+			appGameContainer.setDisplayMode(GAMEWINDOW_X, GAMEWINDOW_Y, FULLSCREENMODE);
+			appGameContainer.start();
+
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Initiates the States needed for the game.
+	 * @param gameContainer
+	 */
+	@Override
+	public void initStatesList(GameContainer gameContainer) throws SlickException {
+//		this.getState(MENU).init(gameContainer, this);
+		this.getState(GAMEPLAY).init(gameContainer, this);
+		
+		this.enterState(GAMEPLAY);
 	}
 
 }
