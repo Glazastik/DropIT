1bb615f68ef3de4de82fb5232d68f2b8adba148d
Anton Myrholm <anton.myrholm@gmail.com>
2012-12-10 16:22:57 +0100

    Fixad utils!
    Signed-off-by: Anton Myrholm <anton.myrholm@gmail.com>

--- Labb3/src/orig2011/v2/GameUtils.java
+++ Labb3/src/orig2011/v2/GameUtils.java
@@ -23,4 +23,19 @@ public class GameUtils {
 		board[pos.getX()][pos.getY()] = tile;
 	}
 
+	/**
+	 * Set the tile on a specified position in the gameboard.
+	 * 
+	 * @param x
+	 *            The x position in the gameboard matrix.
+	 * @param y
+	 *            The x position in the gameboard matrix.
+	 * @param tile
+	 *            The type of tile to paint in specified position
+	 */
+	public static void setGameboardState(final int x, int y,
+			final GameTile tile, GameTile[][] board) {
+		board[x][y] = tile;
+	}
+
 }
