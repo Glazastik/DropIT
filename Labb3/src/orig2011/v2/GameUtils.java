52d310973112186033b57fece4c98525b6f01b5d
Anton Myrholm <anton.myrholm@gmail.com>
2012-12-10 11:28:46 +0100

    Fixat GameUtils!
    Signed-off-by: Anton Myrholm <anton.myrholm@gmail.com>

--- Labb3/src/orig2011/v2/GameUtils.java
+++ Labb3/src/orig2011/v2/GameUtils.java
@@ -1,5 +1,26 @@
 package orig2011.v2;
 
+/**
+ * A class to be used in a static way for handling the games.
+ * 
+ * @author Anton Myrholm
+ * @author Pontus Malm
+ * 
+ */
+
 public class GameUtils {
-//hej	
+
+	/**
+	 * Set the tile on a specified position in the gameboard.
+	 * 
+	 * @param pos
+	 *            The position in the gameboard matrix.
+	 * @param tile
+	 *            The type of tile to paint in specified position
+	 */
+	public static void setGameboardState(final Position pos,
+			final GameTile tile, GameTile[][] board) {
+		board[pos.getX()][pos.getY()] = tile;
+	}
+
 }
