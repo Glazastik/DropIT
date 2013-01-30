60039996a605ee47b5db44a80b6c1992d9cf4425
Malm <Down@dhcp-176087.eduroam.chalmers.se>
2013-01-30 17:41:13 +0100

    Made it completely dependent of Rectangle
    Signed-off-by: Malm

--- PhysicBox/src/se/chalmers/malm/box/Box.java
+++ PhysicBox/src/se/chalmers/malm/box/Box.java
@@ -1,14 +1,8 @@
 package se.chalmers.malm.box;
 
 import java.awt.Color;
-import java.awt.Graphics;
 
 public class Box extends Rectangle {
-	
-	private int side;
-	private double speedX, speedY = 1;
-	
-	private Force force = new Force(0, 0);
 	
 	public Box(int x, int y, int side, Color color) {
 		super(x, y, side, side, color);
